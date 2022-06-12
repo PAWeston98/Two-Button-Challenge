4bd53a0b88c4e8fdc9e4a0aa86e0819c48dee448
PAWeston <peter.weston@me.com>
2022-06-12 16:55:40 -0400

    Initial Commit

---
+++ Two Button Challengs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Button Challengs
+//
+//  Created by Peter Weston on 6/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
